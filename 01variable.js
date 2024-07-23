044881543afd6aa9fa5bf8523017749ffbd52f1e
Vaibhavi_g <34022504+vaibhavig12@users.noreply.github.com>
2024-07-23 12:18:01 +0530

    Add files via upload

---
+++ 01variable.js
@@ -0,0 +1,26 @@
+//task1 Declare a variable using var, assign it a number, and log the value to the console
+var variable =1
+console.log(variable)
+//task2 Declare a variable using let, assign it a string, and log the value to the console
+let day="day"
+console.log(day)
+//task3 Declare a variable using const, assign it a number, and log the value to the console
+ const buy=true
+ console.log(buy)
+ //task4 create a variable of different data types (number string boolean object array) and log each variables type using typeof operator
+ 
+ let day ="Monday"
+ var no=1 
+ const challenge=true
+ const thirtydaysJS={type:"daily Challenge", days:30, mode:"online"}
+let challenges=["30daysJS","dailyUI"]
+console.log("day type "+typeof(day))
+console.log("no type "+typeof(no))
+console.log("challenge type "+typeof(challenge))
+console.log("thirtydaysJS type "+typeof(thirtydaysJS))
+console.log("challenges "+typeof(challenges))
+
+//task 5 declare a variable using let,assign it an initial value ,reassign a new value and log both value to the console
+let anyValue=3.14
+const anyValue=360
+console.log(anyValue)
